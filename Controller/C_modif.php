e180d3dab5230047b82c94d073e07798d569086b
Mattoxx2 <113098381+Mattoxx2@users.noreply.github.com>
2022-09-13 09:53:11 +0200

    Modification C_modif
    - Ajout des conditions suivantes: si la variable matière est déclarée, alors modifier le nom, prénom et rafraîchir la page automatiquement.

--- Controller/C_modif.php
+++ Controller/C_modif.php
@@ -6,7 +6,27 @@ $codeens = filter_input(INPUT_GET, 'codeens');
 $codeetudiant = filter_input(INPUT_GET, 'codeetud');
 
 require_once '../Vue/Modification.php';
+if(isset($_POST['matiere'])){
+    modif_matiere($codemat);
+    ?>
+   <script>
+   window.location.href = "../Controller/C_matiere.php";
+   </script>
+<?php
+}
+if(isset($_POST['nomens']) || isset($_POST['prenomens'])){
+    modif_ens($codeens);
+    ?>
+   <script>
+   window.location.href = "../Controller/C_prof.php";
+   </script>
+<?php
+}if(isset($_POST['nometu']) || isset($_POST['prenometu']) || isset($_POST['date']) || isset($_POST['classe'])){
+    modif_etud($codeetudiant);
+    ?>
+   <script>
+   window.location.href = "../Controller/C_etudiant.php";
+   </script>
+<?php
+}
 
-modif_matiere($codemat);
-modif_ens($codeens);
-modif_etud($codeetudiant);

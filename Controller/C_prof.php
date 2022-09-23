06eafd5732e745a9ad3944984018116915dbb6c3
nicolasdraperi <113093384+nicolasdraperi@users.noreply.github.com>
2022-09-23 09:40:23 +0200

    Add files via upload
    ajout commentaire

--- Controller/C_prof.php
+++ Controller/C_prof.php
@@ -10,12 +10,12 @@ require_once '../Vue/Professeurs.php';
 ?>
 
 <?php
-if (isset($_POST['nom']) and isset($_POST['prenom'])){
-    insert_enseignants();
+if (isset($_POST['nom']) and isset($_POST['prenom'])){ //verifie si les champs ont été remplis//
+    insert_enseignants(); // insere les prof dans la base de donnée
     ?>
 
         <script>
-            window.location.href = "../Controller/C_prof.php";
+            window.location.href = "../Controller/C_prof.php"; // actualise la page apres l'insertion
         </script>
     
     <?php

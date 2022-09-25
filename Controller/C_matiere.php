7897d331edb5cb3a17bb3c770fa4355d8be7bddc
catia <catia@PCcat.home>
2022-09-25 12:58:06 +0200

    Redirection de page apres insertion

--- Controller/C_matiere.php
+++ Controller/C_matiere.php
@@ -2,10 +2,18 @@
 <?php
 require_once '../Modele/Connexion_bd.php';
 require_once '../Vue/Sommaire.php';
-?>
-
-<?php
 $matiere = recupere_matieres();
-insert_matieres();
 require_once '../Vue/Matiere.php';
 ?>
+<?php
+if (isset($_POST['matieres'])) { //Vérifie si les champs ont été remplis//
+insert_matieres(); // Insère les matières dans la base de donnée
+?>
+
+    <script>
+        window.location.href = "../Controller/C_matiere.php"; // Redirige la page après l'insertion
+    </script>
+
+    <?php
+    
+}
